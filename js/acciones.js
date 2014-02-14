cbe9c70aeb4d07a41330827c75d2e54b8bc4e89d
Kame44 <cameliaa@gmail.com>
2014-02-14 12:34:56 -0600

    cambio vibrato

--- js/acciones.js
+++ js/acciones.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
     document.addEventListener("deviceready",function(){    //espera que el documento este listo
 	$('#B1').tap(function(){
 		navigator.notification.beep(1);
-	})//tap B1
-	
+	});//tap B1
+	$('#B1').tap(function(){
+		navigator.notification.vibrate(500);
+	});
 	});   //deviceready
 }); //read
